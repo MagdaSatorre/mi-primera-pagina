53379dff907fb2c34d6b781d6b1998199c76ba0c
MagdaSatorre <146379591+MagdaSatorre@users.noreply.github.com>
2023-09-28 23:14:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,201 @@
+//alert ("Hola este es mi Javascript");
+
+//seleccionar elementos
+//let nombre = "Magdalena";
+    //nombre = "Rosario"
+
+//var nombre1 = "Magdalena";
+
+//const nombre2 = "Magdalena"
+
+//console.log(nombre)
+
+//console.log(nombre1)
+
+//console.log(nombre2)
+
+//practica de condicionales
+
+
+//let pais = "argentina";
+//let nombre = "magdalena"
+
+//console.log(pais);
+//console.log(nombre);
+
+//let id, mail;
+//id = "12345"
+//mail = "correo@correo.com"
+
+//console.log(id);
+//console.log(mail);
+
+//const resultado = 23 * 5;
+
+//console.log(resultado);
+
+//const nombre1 = "magdalena ";
+//const nombre2 = "rosario";
+//const nombrecompleto = nombre1 + nombre2;
+
+//console.log(nombrecompleto);
+
+//let mivariable = "texto";
+//mivariable = 7
+
+//console.log(mivariable);
+
+//cadena de texto
+
+//const nombre4 = "carlos";
+//const parrafo = "este es un 'parrafo'";
+//const parrafo2 = 'este es un "parrafo"';
+//const parrafo3 = 'este es un \'parrafo\'';
+
+//numero
+
+//const numero = 4;
+//const numero1 = -2.56
+
+//boleano
+
+//const usuarioConectado = false;
+//const mayorQue = 1 > 2;
+
+//console.log(mayorQue);
+ 
+//arreys o arreglos
+
+//const arreglo = [1, 10, 5];
+//console.log(arreglo);
+
+//const arreglo1 = ['texto', 456, true, {propiedad: 'valor'}, [1, 2, 3]];
+//console.log(arreglo1);
+
+//objeto
+
+//const persona = {
+  //  nombre: 'Magdalena',
+    //edad: 44,
+   // hijos: {
+     //   nombrehijo: 'agusto',
+       // edad: 16,
+        //nombrehijo2: 'evangelina',
+        //edad2: 13,
+   // },
+    //estadocivil: 'casada',
+//};
+//console.log(persona);
+//console.log(persona.hijos.nombrehijo);
+
+//function
+
+//function hola() {
+  //  console.log('hola')
+//}
+
+//hola();
+
+//condicionales
+//estructura basica de una condicional
+// if (true) {
+// codigo a ejecutar
+//}
+
+//ejemplo 1
+
+//const usuario = {
+    //edad: 27,
+    //pais: 'mexico',
+    //ticket: true,
+//};
+
+//if(usuario.edad > 18){
+  //  console.log('el usuario es mayor de edad, puede entrar al concierto')}
+
+//if(usuario.edad > 18 && usuario.ticket){
+  //  console.log('el usuario puede ingresar');
+//}
+
+//innerhtml
+
+//const titulo = document.querySelector('h3');
+
+//console.log(titulo)
+
+//seleccionar documentos del dom
+
+//innerhtml
+
+//const menu = document.getElementById('menu');
+//console.log(typeof menu.innerHTML);
+
+//innertext
+
+//const sucursañ = document.getElementById('sucursal');
+//console.log(sucursal.innerText);
+
+//const listas = document.getElementsByClassName('lista');
+//console.log(listas[1]);
+
+//queryselector
+
+//const sucursal = document.querySelector('.lista');
+//console.log(sucursal);
+
+//const sucursal1 = document.querySelector('#menu');
+//console.log(sucursal1);
+
+//const sucursal2 = document.querySelector('ul li#donde');
+//console.log(sucursal2);
+
+//getelementbytagname
+
+//const items = document.getElementsByTagName('li');
+//console.log(items);
+
+//const suc = document.getElementsByClassName('header');
+//console.log(suc);
+
+//cambiar diseños con java 
+
+//const lista1 = document.querySelector('.lista');
+//console.log (lista1.style);
+
+//const lista1 = document.querySelector('.lista');
+//lista1.style.backgroundColor = 'white';
+//lista1.style.color = 'red';
+
+//const donde = document.querySelector('#donde')
+//donde.style.color = 'red'
+
+
+//const lista2 = document.getElementById('listado');
+//console.log(lista2);
+//console.log(listado.innerText);
+
+//console.log(listado.textContent);
+
+//console.log(listado.innerHTML); 
+
+//const sucursal = document.getElementById('sucursal');
+//console.log(sucursal.innerText);
+
+//sucursal.innerText = 'franquicia';
+
+//agregar texto nuevo en mi pagina web
+
+//const listado1 = document.getElementById('listado')
+
+//const listanueva = document.createElement('li');
+//listanueva.classList.add('lista')
+//listanueva.innerText = 'PRESUPUESTO';
+
+//listado1.append(listanueva);
+
+
+function mostrarClic() {
+    console.log('clic');
+}
+
+
